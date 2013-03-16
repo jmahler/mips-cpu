4522c80d02344125619f49ab660d883faf8b45bb
Jeremiah Mahler <jmahler@mail.csuchico.edu>
2013-03-16 15:02:26 -0700

    removed obsolete files, fixed overflow in ALU

--- verilog/alu_tb.v
+++ verilog/alu_tb.v
@@ -1,6 +1,20 @@
+/*
+ * alu_tb
+ *
+ * This is a test bench for the alu module.
+ *
+ * When run it will perform unit tests and display
+ * tests that have failed along with summary at the end.
+ *
+ *   $ make
+ *
+ *   $ ./alu_tb
+ *
+ * It also generates output (alu_tb.vcd) suitable for
+ * use with Gtkwave.
+ */
 
-`include "alu1.v"
-`include "alu2.v"
+`include "alu.v"
 
 module alu_tb;
 
@@ -13,9 +27,10 @@ module alu_tb;
 	reg [31:0] a, b;
 	reg [3:0] ctl;
 
-	wire [31:0] out1, out2;
-	wire z1, z2;
+	wire [31:0] out1;
+	wire z1;
 
+	// operation control signals
 	localparam [3:0]
 		CTL_ADD = 4'b0010,
 		CTL_AND = 4'b0000,
@@ -25,7 +40,7 @@ module alu_tb;
 		CTL_SUB = 4'b0110,
 		CTL_XOR = 4'b1101;
 
-	// test results
+	// perform a test, a calculate results
 	task t;
 		input [31:0] out1;
 		input [31:0] out2;
@@ -51,8 +66,7 @@ module alu_tb;
 	endtask
 
 	// both ALU's will be tested, they should have identical results
-	alu1 alu1(.ctl(ctl), .a(a), .b(b), .out(out1), .z(z1));
-	alu2 alu2(.ctl(ctl), .a(a), .b(b), .out(out2), .z(z2));
+	alu alu1(.ctl(ctl), .a(a), .b(b), .out(out1), .z(z1));
 
 	always begin
 		clk <= ~clk;
@@ -68,59 +82,70 @@ module alu_tb;
 
 		clk <= 1'b0;
 
-		/*
+		// 2 + 2 = 4
+		a <= 2;
+		b <= 2;
+		ctl <= CTL_ADD;
+		@(posedge clk);
+		t(out1, 4, z1, 0);
+
+		// 4 - 2 = 2
+		a <= 4;
+		b <= 2;
+		ctl <= CTL_SUB;
+		@(posedge clk);
+		t(out1, 2, z1, 0);
+
 		a <= 15;
 		b <= 126;
 		ctl <= CTL_SUB;
 		@(posedge clk);
-		t(out1, out2, z1, z2);
-
-		a <= 15;
-		b <= 126;
-		ctl <= CTL_NOR;
-		@(posedge clk);
-		t(out1, out2, z1, z2);
+		t(out1, -111, z1, 0);
 
 		a <= 32'hffff1010;
 		b <= 32'h0000ffff;
 		ctl <= CTL_OR;
 		@(posedge clk);
-		t(out1, out2, z1, z2);
+		t(out1, 32'hffffffff, z1, 0);
+
+		a <= 32'hffff1010;
+		b <= 32'h0000ffff;
+		ctl <= CTL_NOR;
+		@(posedge clk);
+		t(out1, 32'h00000000, z1, 1);
+
+		a <= 32'hffff1010;
+		b <= 32'h0000ffff;
+		ctl <= CTL_AND;
+		@(posedge clk);
+		t(out1, 32'h00001010, z1, 0);
 
 		a <= 100000;
 		b <= 10001;
 		ctl <= CTL_SLT;
 		@(posedge clk);
-		t(out1, out2, z1, z2);
-		*/
+		t(out1, 0, z1, 1);
 
 		// works when less than (slt) is based on sign bit
 		a <= -7;
 		b <= 6;
 		ctl <= CTL_SLT;
 		@(posedge clk);
-		t(out1, 32'h1, z1, 0);
-		t(out2, out1, z2, z1);
+		t(out1, 1, z1, 0);
 
 		// Overflows in the signed number domain.
-		a <= 32'h4a1ba35d;
-		b <= 32'h98782a64;
+		a <= 32'h4a1ba35d;  // 1243325277
+		b <= 32'h98782a64;  // -1736955292
 		ctl <= CTL_SLT;
 		@(posedge clk);
-		t(out1, 32'h0, z1, 1);
-		t(out2, out1, z2, z1);
+		t(out1, 32'h0, z1, 1);  // a !< b
 
-		// wrong in simulation, wrong in Logisim
-		// 2^32 = 4294967296
-		// 2^32/2 = 2147483648
-		//
-		// 2106327511 - (-1303574716) = 3409902227 (negative bit set!)
+		// 2106327511 - (-1303574716) = 3409902227 (negative bit set)
 		a <= 32'h7d8c01d7;  // 2106327511
 		b <= 32'hb24d0744;  // -1303574716
 		ctl <= CTL_SLT;
 		@(posedge clk);
-		t(out2, 32'h0, z2, 1);
-		t(out2, out1, z2, z1);
+		t(out1, 32'h0, z1, 1);
 
 		// another sign overflow example
 		a <= 32'ha1a538c4;
@@ -128,7 +153,6 @@ module alu_tb;
 		ctl <= CTL_SLT;
 		@(posedge clk);
 		t(out1, 32'h1, z1, 0);
-		t(out2, out1, z2, z1);
 
 		// and yet another
 		a <= 32'h4270aa12;
@@ -136,51 +160,12 @@ module alu_tb;
 		ctl <= CTL_SLT;
 		@(posedge clk);
 		t(out1, 32'h0, z1, 1);
-		t(out2, out1, z2, z1);
 
 		@(posedge clk);
 		a <= 1010100;
-		b <= 100000;
+		b <= 0100000;
 		ctl <= CTL_XOR;
-		t(out1, out2, z1, z2);
-
-/*
-		for (i = 0; i <= 1517*10; i = i + 1517) begin
-			for (j = 10100; j <= 10100 + 1135*10; j = j + 1135) begin
-
-				a <= i;
-				b <= j;
-
-				ctl <= CTL_ADD;
-				@(posedge clk);
-				t(out1, out2, z1, z2);
-
-				ctl <= CTL_AND;
-				@(posedge clk);
-				t(out1, out2, z1, z2);
-
-				ctl <= CTL_NOR;
-				@(posedge clk);
-				t(out1, out2, z1, z2);
-
-				ctl <= CTL_OR;
-				@(posedge clk);
-				t(out1, out2, z1, z2);
-
-				ctl <= CTL_SLT;
-				@(posedge clk);
-				t(out1, out2, z1, z2);
-
-				ctl <= CTL_SUB;
-				@(posedge clk);
-				t(out1, out2, z1, z2);
-
-				ctl <= CTL_XOR;
-				@(posedge clk);
-				t(out1, out2, z1, z2);
-			end
-		end
-*/
+		t(out1, 0, z1, 1);
 
 		// add some space at the end of the timing diagram
 		@(posedge clk);
