8adc98e4365c5b3a5e91cb4c7a64f34472145c7f
Jeremiah Mahler <jmahler@mail.csuchico.edu>
2013-04-26 12:43:41 -0700

    added test bench for load use data hazard (stall required)

---
+++ five_stage/verilog/test/04-stall/main.asm
@@ -0,0 +1,20 @@
+
+addi $1, $zero, 5
+addi $2, $zero, 7
+addi $3, $zero, 0
+
+add $zero, $zero, $zero		# nop
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+
+sw $1, 0($zero)  # 5
+sw $2, 4($zero)  # 7
+
+add $zero, $zero, $zero		# nop
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+
+lw $3, 4($zero)  # 7
+add $4, $1, $3   # 12
+
+halt
