46edf0ded30f07c51c5865f602eeffd88b897ed8
Jeremiah Mahler <jmmahler@gmail.com>
2014-11-04 12:13:31 -0800

    assemble using Gcc
    Previously, the test code was assembled using a custom assembler which
    was not included with this project.  This made it difficult to try out
    new code when the assembler was not available.  Improve this situation
    by assembling the code using a standard Gcc MIPS assembler.
    
    Compilation was tested using the MIPS cross compiler available in
    Debian (binutils-mips-linux-gnu).  An additional program, bin2hex.c, was
    created to convert the binary dump to ASCII hex format suitable for use
    with a Verilog simulator.

---
+++ verilog/test/bin2hex.c
@@ -0,0 +1,72 @@
+/*
+ * NAME
+ *
+ * bin2hex
+ *
+ * DESCRIPTION
+ *
+ * Given a binary file, convert each byte to ascii hex, with one byte
+ * per line.
+ *
+ *   bin2hex t0001-no_hazard.elf > t0001-no_hazard.hex
+ *
+ * Verilog simulators can read ascii hex values but not binary.  This
+ * program does this conversion so it can be used with Verilog.
+ *
+ * AUTHOR
+ *
+ * Jeremiah Mahler <jmmahler@gmail.com>
+ *
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	uint8_t byte;
+	int inputfd;
+	ssize_t len;
+	int n;
+	int byte_nl = 4;  /* break with newline every 4 bytes */
+
+	if (2 != argc) {
+		fprintf(stderr, "usage: %s <bin file> > <out file>\n", argv[0]);
+		return 1;
+	}
+
+	inputfd = open(argv[1], O_RDONLY);
+	if (inputfd < 0) {
+		perror("open");
+		return 1;
+	}
+
+	n = 1;
+	while (1) {
+		len = read(inputfd, &byte, sizeof(byte));
+		if (len < 0) {
+			perror("read");
+			return 1;
+		} else if (0 == len) {
+			break;
+		}
+
+		printf("%02X", byte);
+
+		/* add newline every byte_nl bytes */
+		if (0 == n % byte_nl) {
+			printf("\n");
+		}
+
+		n++;
+	}
+
+	close(inputfd);
+
+	return 0;
+}
