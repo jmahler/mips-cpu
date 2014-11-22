1c1c3eec1105aca96e1ba2c70df4da8445875343
Jeremiah Mahler <jmmahler@gmail.com>
2014-11-22 01:14:32 -0800

    add new fv branch test, fails with bne
    Added a new final value branch test.  It checks both beq and bne but
    it fails with bne.  The cause has yet to be determined.

---
+++ verilog/test/t0005-branch.fv.asm
@@ -0,0 +1,37 @@
+
+#
+# t0005-branch.fv.asm
+#
+# Perform branches (beq, bne) which would create hazards
+# and see if they are handled correctly.
+#
+
+# initial values
+addi $t0, $zero, 1
+addi $t1, $zero, 2
+
+# increment $t0 so it equals $t1
+addi $t0, $t0, 1
+
+beq $t0, $t1, skip1
+
+# these shouldn't get added
+#  If they do, the third hex digit will be 1
+addi $t0, $t0, 256
+addi $t1, $t1, 256
+
+skip1:
+
+add $t0, $t0, $t1  # 2 + 2 = 4
+add $t1, $t0, $t1  # 4 + 2 = 6
+
+bne $t0, $t1, skip2
+
+# these shouldn't get added
+#  If they do, the fourth hex digit will be 1
+addi $t0, $t0, 4096
+addi $t1, $t1, 4096
+
+skip2:
+
+# $t0 = 4, $t1 = 6
