6f6d008172de10cb5342f2d760a0b18c9ea65811
Jeremiah Mahler <jmmahler@gmail.com>
2014-11-24 00:57:55 -0800

    more elaborate jump test
    Make the jump test more elaborate with multiple jumps instead
    of just one.

--- verilog/test/t0006-jump.fv.asm
+++ verilog/test/t0006-jump.fv.asm
@@ -6,11 +6,21 @@
 # initial values
 addi $t0, $zero, 1
 
-j skip_add
+j skip1
 
 # increment $t0 so it equals $t1
 addi $t0, $t0, 256
 
-skip_add:
+skip1:
 
-# $t0 = 1
+addi $t0, $t0, 1
+
+j skip2
+
+addi $t0, $t0, 512
+
+skip2:
+
+addi $t0, $t0, 1
+
+# $t0 = 3
