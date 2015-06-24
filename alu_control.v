278cb02d2d81501741ce2960cd58bbb36b953ad8
Jeremiah Mahler <jmmahler@gmail.com>
2015-06-24 16:41:12 -0700

    use simpler module argument format
    Use the simpler module argument format, which includes the input/output
    and size in one place, instead of separating them in to two places.

--- alu_control.v
+++ alu_control.v
@@ -1,11 +1,10 @@
 `ifndef _alu_control
 `define _alu_control
 
-module alu_control(funct, aluop, aluctl);
-
-	input wire [5:0] funct;
-	input wire [1:0] aluop;
-	output reg [3:0] aluctl;
+module alu_control(
+		input wire [5:0] funct,
+		input wire [1:0] aluop,
+		output reg [3:0] aluctl);
 
 	reg [3:0] _funct;
 
